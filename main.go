ace9adf9278f7139dae5e511deb60b967ca4777b
leeningli <1507045477@qq.com>
2018-12-13 18:00:08 +0800

    register to eureka

--- main.go
+++ main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/Unknwon/goconfig"
+	"github.com/hudl/fargo"
 
 	service "github.com/leeningli/backing-fulfillment/service"
 )
 
+var (
+	eureka_url string
+	port       string
+)
+
 func main() {
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "3001"
-	}
+	intPort, _ := strconv.Atoi(port)
 
 	server := service.NewServer()
-	server.Run(":" + port)
+	go server.Run(":" + port)
+	fmt.Println("12334")
+	c := fargo.NewConn(eureka_url + "/eureka")
+	i := fargo.Instance{
+		HostName:         "lee",
+		Port:             intPort,
+		App:              "TestEurekaApp",
+		IPAddr:           "127.0.0.1",
+		VipAddress:       "127.0.0.1",
+		SecureVipAddress: "127.0.0.1",
+		DataCenterInfo:   fargo.DataCenterInfo{Name: fargo.MyOwn},
+		Status:           fargo.UP,
+	}
+	c.RegisterInstance(&i)
+	f, _ := c.GetApps()
+	for key, theApp := range f {
+		fmt.Println("App:", key, "First Host Name:", theApp.Instances[0].HostName)
+	}
+	app, _ := c.GetApp("TestEurekaApp")
+	fmt.Printf("my app info is %v\n", app)
+}
+
+func init() {
+	cfg, err := goconfig.LoadConfigFile("config.ini")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	eureka_url, err = cfg.GetValue("eureka", "url")
+	if err != nil {
+		fmt.Println(err)
+		eureka_url = "http://127.0.0.1:8080" //docker run -d -it -p 8080:8761 springcloud/eureka-------http://127.0.0.1:8080/eureka/apps/TestEurekaApp
+	}
+	port, err = cfg.GetValue("main", "port")
+	if err != nil {
+		fmt.Println(err)
+		port = "3001"
+	}
 }
